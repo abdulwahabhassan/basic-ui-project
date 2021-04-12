cc6d00e7bfda7d6207cc6116013a3c874efe8725
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-04-12 23:27:38 +0100

    Initial Commit

--- app/src/androidTest/java/com/decagon/profile/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/decagon/profile/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.decagon.profile", appContext.packageName)
+        assertEquals("com.decagon.profilephoto", appContext.packageName)
     }
 }
--- app/src/main/java/com/decagon/profile/MainActivity.kt
+++ app/src/main/java/com/decagon/profile/MainActivity.kt
@@ -2,10 +2,27 @@ package com.decagon.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var switch: Switch = findViewById(R.id.switchButton)
+
+        switch.setOnClickListener() {
+
+            if (switch.isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
