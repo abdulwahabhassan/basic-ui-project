d4a7b6d62396590e88b55c233b2d091c5906ccf2
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-04-15 11:26:36 +0100

    Changed some colour references and made some comments where necessary

--- app/src/main/java/com/decagon/profile/MainActivity.kt
+++ app/src/main/java/com/decagon/profile/MainActivity.kt
@@ -12,16 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //sets the view for this activity to the specified layout
 
-        var switch: Switch = findViewById(R.id.switchButton)
+        val switch: Switch = findViewById(R.id.switchButton) //This finds the switch view for toggling between night mode and light mode in the xml layout
 
-        switch.setOnClickListener() {
+        switch.setOnClickListener() { //This sets an onClickListener to the switch view
 
             if (switch.isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //If switch view is checked, Night mode comes on
             } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //else Night mode is off and light mode comes on
             }
         }
     }
